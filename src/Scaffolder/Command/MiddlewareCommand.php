2d92ddb9cb0edfafabbd8d6f446ae806b8de1a89
valentin v / vvval <valentin@spiralscout.com>
2019-08-20 15:05:51 +0300

    update controller command

--- src/Scaffolder/Command/MiddlewareCommand.php
+++ src/Scaffolder/Command/MiddlewareCommand.php
@@ -38,6 +38,7 @@ class MiddlewareCommand extends AbstractCommand
     {
         /** @var MiddlewareDeclaration $declaration */
         $declaration = $this->createDeclaration();
+
         $this->writeDeclaration($declaration);
     }
 }
