9f7bea29f0804171e588175f4db6e8fa918e2eb4
valentin v / vvval <valentin@spiralscout.com>
2019-08-22 10:28:34 +0300

    add namespaced name test

---
+++ tests/Scaffolder/Command/NamespacedNameTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Spiral Framework. Scaffolder
+ *
+ * @license MIT
+ * @author  Valentin V (vvval)
+ */
+declare(strict_types=1);
+
+namespace Spiral\Tests\Scaffolder\Command;
+
+class NamespacedNameTest extends AbstractCommandTest
+{
+    private const CLASS_NAME = '\\TestApp\\Controller\\Namespaced\\SampleController';
+
+    public function tearDown(): void
+    {
+        $this->deleteDeclaration(self::CLASS_NAME);
+    }
+
+    /**
+     * @throws \ReflectionException
+     * @throws \Throwable
+     */
+    public function testScaffold(): void
+    {
+        $this->console()->run('create:controller', [
+            'name'      => 'namespaced/sample',
+            '--comment' => 'Sample Controller',
+            '-a'        => ['index', 'save']
+        ]);
+
+        clearstatcache();
+        $this->assertTrue(class_exists(self::CLASS_NAME));
+
+        $reflection = new \ReflectionClass(self::CLASS_NAME);
+
+        $this->assertStringContainsString('Sample Controller', $reflection->getDocComment());
+        $this->assertTrue($reflection->hasMethod('index'));
+        $this->assertTrue($reflection->hasMethod('save'));
+    }
+}
