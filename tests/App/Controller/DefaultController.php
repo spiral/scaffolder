e56631a31cebbf76ee3ad3b52441dec4e4ff9e0f
spiralbot <ci-bot@spiralscout.com>
2026-08-05 12:05:47 +0000

    prepare release

--- tests/App/Controller/DefaultController.php
+++ tests/App/Controller/DefaultController.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 namespace Spiral\Tests\Scaffolder\App\Controller;
 
-final class DefaultController {}
+class DefaultController {}
--- tests/App/TestApp.php
+++ tests/App/TestApp.php
@@ -9,7 +9,7 @@ use Spiral\Core\Container;
 use Spiral\Scaffolder;
 use Spiral\Validation\Bootloader\ValidationBootloader;
 
-final class TestApp extends Boot\AbstractKernel
+class TestApp extends Boot\AbstractKernel
 {
     protected const LOAD = [
         Scaffolder\Bootloader\ScaffolderBootloader::class,
